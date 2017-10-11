360e29cb26a8b8c4d2c5796a62fc9ec1466fc839
xvenge00 <jawe1222@gmail.com>
2017-10-11 15:19:35 +0200

    Trochu upravene preklepy

--- pgrep.cpp
+++ pgrep.cpp
@@ -1,4 +1,4 @@
-//Adam Venge xvenge00
+//Adam Venger xvenge00
 
 /**
 *  Prvy projekt na IPS spolocna praca 
@@ -20,7 +20,7 @@ char *line;
 int score;
 int end = 1;
 
-std::mutex res_thread, laod_line;
+std::mutex res_thread, load_line;
 
 unsigned working;
 
@@ -33,18 +33,18 @@ char *read_line(int *res) {
     std::string line;
     char *str;
     if (std::getline(std::cin, line)) {
-        str=(char *) malloc(sizeof(char)*(line.length()+1));
-        strcpy(str,line.c_str());
-        *res=1;
+        str = (char *) malloc(sizeof(char) * (line.length() + 1));
+        strcpy(str, line.c_str());
+        *res = 1;
         return str;
     } else {
-        *res=0;
+        *res = 0;
         return NULL;
     }
 
 }
 
-void f(int ID){
+void f(int ID) {
     std::mutex *my_mutex;
     std::regex *my_regex;
     int my_score;
@@ -56,43 +56,43 @@ void f(int ID){
 
 
     int res = 0;
-    while (1){
-        while (!my_mutex->try_lock()){
-            if (!end){
+    while (1) {
+        while (!my_mutex->try_lock()) {
+            if (!end) {
                 return;
             }
         }
 
         res = std::regex_match(line, (*my_regex));
         res_thread.lock();
-        score += res?my_score:0;
+        score += res ? my_score : 0;
         working -= 1;
-        if (working == 0){
-            laod_line.unlock();
+        if (working == 0) {
+            load_line.unlock();
         }
         res_thread.unlock();
     }
 }
 
 int main(int argc, char **argv) {
-    laod_line.lock();
+    load_line.lock();
     /*************************
      * deklaracia premennych
      */
     unsigned num = 0; // pocet regex
     int minimum;
-    std::vector<std::thread *> thread;
+    std::vector < std::thread * > thread;
 
     /**************************
      * kontroala vstupu a init
      */
 
-    if ((argc%2 != 0) || (argc < 4)){
-        fprintf(stderr,"Zle zadane argumenty\n");
+    if ((argc % 2 != 0) || (argc < 4)) {
+        fprintf(stderr, "Zle zadane argumenty\n");
         return 1;
     }
-    num = (argc - 2)/2;
-    minimum = strtod(argv[1],NULL); //todo skontrolovat argv[1] ci je cislo
+    num = (argc - 2) / 2;
+    minimum = strtod(argv[1], NULL); //todo skontrolovat argv[1] ci je cislo
     // nastavenie velikosti poli
     zamky.resize(num);
     thread.resize(num);
@@ -100,10 +100,10 @@ int main(int argc, char **argv) {
     reg.resize(num);
 
     // inicializovanie poli
-    for (unsigned i=0; i<num; i++){
+    for (unsigned i = 0; i < num; i++) {
         std::mutex *new_mutex = new std::mutex();
-        std::regex *new_regex = new std::regex(argv[2+2*i]);
-        int new_score = (int) strtod(argv[3+2*i],NULL); //todo skontrolovat ci retazec je cislo
+        std::regex *new_regex = new std::regex(argv[2 + 2 * i]);
+        int new_score = (int) strtod(argv[3 + 2 * i], NULL); //todo skontrolovat ci retazec je cislo
         //zamknutie mutex
         new_mutex->try_lock();
         //nastavenie poli
@@ -112,24 +112,24 @@ int main(int argc, char **argv) {
         min_score[i] = new_score;
     }
     // spustenie threadou
-    for (unsigned i=0; i<num; i++){
-        std::thread *new_thread = new std::thread(f,i);
+    for (unsigned i = 0; i < num; i++) {
+        std::thread *new_thread = new std::thread(f, i);
         thread[i] = new_thread;
     }
     /**********************************
      * Vlastni vypocet pgrep
      * ********************************/
     line = read_line(&end);
-    while (end){
+    while (end) {
         score = 0;
         working = num;
 
-        for (unsigned i=0; i<num; i++){
+        for (unsigned i = 0; i < num; i++) {
             zamky[i]->unlock();
         }
-        laod_line.lock(); //todo deadlock
-        if (minimum <= score){
-            printf("%s\n",line);
+        load_line.lock(); //todo deadlock
+        if (minimum <= score) {
+            printf("%s\n", line);
         }
         free(line);
         line = read_line(&end);
@@ -139,7 +139,7 @@ int main(int argc, char **argv) {
      * Uvolneni pameti
      * ********************************/
 
-    for (unsigned i=0; i<num; i++){
+    for (unsigned i = 0; i < num; i++) {
         (*(thread[i])).join();
         delete thread[i];
         delete zamky[i];
